ce7422d31ad2664da0b64e5eb5a5c799c136d3d0
ueno <tomoko.essvivh@gmail.com>
2025-05-29 12:12:12 +0900

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+
+  const fade = document.querySelectorAll(".fade");
+});
